99b14ebd650f2694c168c0fcf951bc6b094e9697
hdkoh <hd.koh290293@gmail.com>
2023-09-11 19:33:55 +0800

    Initial Commit

---
+++ routes/square.py
@@ -0,0 +1,18 @@
+import json
+import logging
+
+from flask import request
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/square', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    input_value = data.get("input")
+    result = input_value * input_value
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
