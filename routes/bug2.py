9eca066014e87b310badf86e07df079e875b4242
MartinCKH <116894103+MartinCKH@users.noreply.github.com>
2024-09-28 15:32:13 +0800

    doge bullet seems make sense

--- routes/bug2.py
+++ routes/bug2.py
@@ -1,52 +1,53 @@
-import heapq
+# import heapq
 
-import json
-import logging
+# import json
+# import logging
 
-from flask import request
+# from flask import request
 
-from routes import app
+# from routes import app
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-def max_bugsfixed(bugseq):
-    # Sort bugs by their escalation limits
-    bugseq_list = bugseq["bugseq"]
-    # bugseq[bugseq].sort(key=lambda x: x[1])
-    bugseq = sorted(bugseq_list, key=lambda x: x[1])
+# def max_bugsfixed(bugseq):
+#     # Sort bugs by their escalation limits
+#     bugseq_list = bugseq["bugseq"]
+#     # bugseq[bugseq].sort(key=lambda x: x[1])
+#     bugseq = sorted(bugseq_list, key=lambda x: x[1])
     
-    current_time = 0
-    max_heap = []
+#     current_time = 0
+#     max_heap = []
     
-    for difficulty, limit in bugseq:
-        if current_time + difficulty <= limit:
-            heapq.heappush(max_heap, -difficulty)
-            current_time += difficulty
-        elif max_heap and -max_heap[0] > difficulty:
-            current_time += difficulty + heapq.heappop(max_heap)
-            heapq.heappush(max_heap, -difficulty)
+#     for difficulty, limit in bugseq:
+#         if current_time + difficulty <= limit:
+#             heapq.heappush(max_heap, -difficulty)
+#             current_time += difficulty
+#         elif max_heap and -max_heap[0] > difficulty:
+#             current_time += difficulty + heapq.heappop(max_heap)
+#             heapq.heappush(max_heap, -difficulty)
 
-    return len(max_heap)
-
-
-@app.route('/bugfixer/p2', methods=['POST'])
-def bug2():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    # result = max_bugsfixed(data)
-
-    result = [max_bugsfixed(p) for p in data]
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
-
-# bugseq = [[20, 30], [30, 150], [110, 135], [210, 330]]
-# print(max_bugsfixed(bugseq))  # Output: 3
+#     return len(max_heap)
 
 
-# input An array of objects
-[
-    {
-        "bugseq": [[20,30],[30,150],[110,135],[210,330]]
-    },
+# @app.route('/bugfixer/p2', methods=['POST'])
+# def bug2():
+#     data = request.get_json()
+#     logging.info("data sent for evaluation {}".format(data))
+#     # result = max_bugsfixed(data)
 
-]
+#     result = [max_bugsfixed(p) for p in data]
+#     logging.info("My result :{}".format(result))
+#     return json.dumps(result)
+
+# # bugseq = [[20, 30], [30, 150], [110, 135], [210, 330]]
+# # print(max_bugsfixed(bugseq))  # Output: 3
+
+
+# # input An array of objects
+# [
+#     {
+#         "bugseq": [[20,30],[30,150],[110,135],[210,330]]
+#     },
+
+# ]
+
