94a9d2ffd9125c0400863e16b59aeb262f94f076
starfall chan <tr246135@gmail.com>
2024-09-28 15:03:21 +0800

    added mini_interpreter

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-import routes.square
+# import routes.square
 # import routes.wordle
 # import routes.monsterHunter
 # import routes.Klotski
 # import routes.bug1
 # import routes.bug2
-import routes.Digital_Colony # Yiu
+# import routes.Digital_Colony # Yiu
+import routes.mini_interepter
