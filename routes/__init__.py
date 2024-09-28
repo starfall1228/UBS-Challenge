7777aebddb7ead8a3194bb0c1a5db49cbb60c29e
starfall chan <tr246135@gmail.com>
2024-09-28 11:06:48 +0800

    added monsterHunter

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,3 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 import routes.square
 import routes.wordle
+import routes.monsterHunter
---
+++ routes/monsterHunter.py
@@ -0,0 +1,39 @@
+import json
+import logging
+
+from flask import request
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+def check_efficiency(monsters, wave, attackable, force_rest):
+    # last wave
+    if wave == len(monsters) - 1:
+        if attackable: 
+            return monsters[wave]
+        else:
+            return 0
+    else:
+        rest_efficiency = check_efficiency(monsters, wave + 1, attackable, False)
+        if force_rest:
+            return rest_efficiency
+
+        if attackable:
+            attack_efficiency = monsters[wave] * 1 + check_efficiency(monsters, wave + 1, False, True)
+        else:
+            attack_efficiency = 0
+
+        prepare_efficiency = monsters[wave] * -1 + check_efficiency(monsters, wave + 1, True, False)
+
+        return max(rest_efficiency, attack_efficiency, prepare_efficiency)
+
+@app.route('/efficient-hunter-kazuma', methods=['POST'])
+def hunter_kazuma():
+    datas = request.get_json()
+    logging.info("data sent for evaluation {}".format(datas))
+
+    efficiency = [{"efficiency": check_efficiency(data["monsters"], 0, False, False)} for data in datas]
+
+    logging.info("My result :{}".format(efficiency))
+    return json.dumps(efficiency)
