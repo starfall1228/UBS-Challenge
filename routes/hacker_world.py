4a1b26397d3aff3e5b7594bd05031b58d480e7f9
MartinCKH <116894103+MartinCKH@users.noreply.github.com>
2024-09-29 04:23:00 +0800

    update hack

--- routes/hacker_world.py
+++ routes/hacker_world.py
@@ -13,18 +13,18 @@ def coolcodehack():
         'username': '123456777',  # Replace with your actual username
         'password': 'Abcd!234'   # Replace with your actual password
     }
-    # Send a POST request to the external API
-    url = 'https://api.crazy-collectors.com/coolcode/api/assignment/score'
-    payload = {
-        'username': 'sJH1Fe',  # Replace with the actual username
-        'assignmentId': 3,             # Replace with the actual assignment ID
-        'score': 100                     # Replace with the desired score
-    }
-    api_response = requests.post(url, json=payload)
+    # # Send a POST request to the external API
+    # url = 'https://api.crazy-collectors.com/coolcode/api/assignment/score'
+    # payload = {
+    #     'username': 'sJH1Fe',  # Replace with the actual username
+    #     'assignmentId': 3,             # Replace with the actual assignment ID
+    #     'score': 100                     # Replace with the desired score
+    # }
+    # api_response = requests.post(url, json=payload)
 
-    # Print the response from the external API
-    print(api_response.status_code)
-    print(api_response.json())
+    # # Print the response from the external API
+    # print(api_response.status_code)
+    # print(api_response.json())
 
     return jsonify(response)
     # return jsonify(response)
