0e68c17c4ef7b71e1ede62716be9628e9f75ee3a
MartinCKH <116894103+MartinCKH@users.noreply.github.com>
2024-09-28 15:47:13 +0800

    comment out other module

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 # import routes.square
-import routes.wordle
+# import routes.wordle
 # import routes.monsterHunter
 # import routes.Klotski
 # import routes.bug1
 # import routes.bug2
 # import routes.Digital_Colony # Yiu
-import routes.mini_interepter
+# import routes.mini_interepter
 import routes.dodge
 
